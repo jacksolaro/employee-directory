bb3f93f4b8220d30e7068678b22519ba01940af0
jacksolaro <jacksolaro@gmail.com>
2020-11-06 19:49:34 -0800

    added filter search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import EmployeeTable from './components/EmployeeTable';
+import SearchForm from './components/Search';
 
 function App() {
   return (
     <div className="">
-      <div className="jumbotron jumbotron-fluid">
+      <div className="jumbotron jumbotron-fluid mb-0">
         <div className="container">
           <h1 className="display-4">EMPLOYEE DIRECTORY</h1>
           <p className="lead">Looking for an employee? We've got some.</p>
--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -1,10 +1,13 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import API from "../utils/Api.js";
+import SearchForm from "./Search.js";
 
 class EmployeeTable extends Component {
     state = {
         employees: [],
-        isSorted: false
+        filteredEmployees: [],
+        isSorted: false,
+        search: ""
     }
 
     componentDidMount = () => {
@@ -18,32 +21,49 @@ class EmployeeTable extends Component {
     sortEmployees = () => {
         let sortedEmployees = [...this.state.employees]
 
-        if(this.state.isSorted){
+        if (this.state.isSorted) {
             sortedEmployees.reverse();
         } else {
-            sortedEmployees.sort((a,b) => (a.email > b.email) ? 1 : ((b.email > a.email) ? -1 : 0));
+            sortedEmployees.sort((a, b) => (a.email > b.email) ? 1 : ((b.email > a.email) ? -1 : 0));
         }
-        this.setState({employees: sortedEmployees, isSorted: true})
+        this.setState({ employees: sortedEmployees, isSorted: true })
     }
+
+    handleInputChange = event => {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            [name]: value
+        });
+    };
 
     render() {
         return (
             <div>
+                <div className="bg-dark text-white">
+                    <div className="container">
+                        <SearchForm
+                            value={this.state.search}
+                            handleInputChange={this.handleInputChange}
+                            handleFormSubmit={this.handleFormSubmit}>
+                        </SearchForm>
+                    </div>
+                </div>
                 <table className="table table-striped">
                     <thead>
                         <tr>
                             <th scope="col">ID Photo</th>
-                            <th scope="col"><button>First</button></th>
+                            <th scope="col">First</th>
                             <th scope="col">Last</th>
-                            <th scope="col"><button onClick={() => this.sortEmployees()}>Email</button></th>
+                            <th scope="col"><button className="btn btn-link text-black" onClick={() => this.sortEmployees()}>Email</button></th>
                             <th scope="col">Phone</th>
                             <th scope="col">Cell</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {this.state.employees.map(employee => (
+                        {this.state.employees.filter(employee => employee.name.first.includes(this.state.search)).map(employee => (
                             <tr key={employee.id.value}>
-                                <td><img src={employee.picture.medium} alt="sample employee ID"/></td>
+                                <td><img src={employee.picture.medium} alt="sample employee ID" /></td>
                                 <td>{employee.name.first}</td>
                                 <td>{employee.name.last}</td>
                                 <td>{employee.email}</td>
